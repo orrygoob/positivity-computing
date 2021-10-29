cb00e5a53414ef73ccbc2e844d89e7ccbf995f61
Amren Stephenson <92263829+amrenstephenson@users.noreply.github.com>
2021-10-29 12:39:05 +0100

    Improvements to page loading

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
+"use strict";
+const VALID_PAGES = ["home", "environment", "health", "history", "wildlife"]; // We don't want them loading any old file with a custom location.hash!
+
 function loadPage(pageId) {
-    const VALID_PAGES = ["home", "environment", "health", "history", "wildlife"]; // We don't want them loading any old file with a custom location.hash!
+    
     if (VALID_PAGES.indexOf(pageId) != -1) {
         $("#content-div").load(`page-${pageId}.html`);
     }
 }
 
+function checkHashLocation() {
+    if (location.hash == "" || VALID_PAGES.indexOf(location.hash.slice(1)) == -1) {
+        location.hash = "#home";
+    }
+}
+
 $( document ).ready(function() {
     $(window).on('hashchange',function(){ 
+        checkHashLocation();
         loadPage(location.hash.slice(1));
     });
-
-    if (location.hash.slice(1) == "") {
-        location.hash = "#home"
-    }
+    
+    checkHashLocation();
+    loadPage(location.hash.slice(1));
 });
 
