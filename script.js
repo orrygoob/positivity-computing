b6b42960cbd2fe3e31f6ad7aa264cf44ffdefe70
Amren Stephenson <92263829+amrenstephenson@users.noreply.github.com>
2021-10-29 12:27:41 +0100

    Load pages with script. Closes #9

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Some javascript
+function loadPage(pageId) {
+    const VALID_PAGES = ["home", "environment", "health", "history", "wildlife"]; // We don't want them loading any old file with a custom location.hash!
+    if (VALID_PAGES.indexOf(pageId) != -1) {
+        $("#content-div").load(`page-${pageId}.html`);
+    }
+}
+
+$( document ).ready(function() {
+    $(window).on('hashchange',function(){ 
+        loadPage(location.hash.slice(1));
+    });
+
+    if (location.hash.slice(1) == "") {
+        location.hash = "#home"
+    }
+});
+
